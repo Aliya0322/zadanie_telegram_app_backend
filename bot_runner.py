e9d0486644e94b9f33d5fce5c34fb508d4e05ced
Алия Дюсембекова <DusembekovaAZ@gmail.com>
2025-11-30 10:01:31 +0300

    Fix aiogram 3.x compatibility: remove DefaultBotProperties import

--- bot_runner.py
+++ bot_runner.py
@@ -5,7 +5,6 @@
 import asyncio
 import logging
 from aiogram import Bot
-from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from config import settings
 from bot_handler import create_dispatcher, set_bot_commands
@@ -26,7 +25,7 @@ async def main():
     # Создаем бота
     bot = Bot(
         token=settings.bot_token,
-        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+        parse_mode=ParseMode.HTML
     )
     
     # Устанавливаем команды бота
